4a2340dec8f07f30903bfeeb7970d05b7f2cad5d
alex <alexvetrici@gmail.com>
2023-06-22 15:59:36 +0200

    bubbleSort function added

---
+++ sorting/bubble-sort/bubble-sort.py
@@ -0,0 +1,9 @@
+def bubbleSort(iterable):
+    for i in range(len(iterable) - 1):
+    
+        for k in range(0, len(iterable) - i - 1):
+    
+            if iterable[k] > iterable[k + 1]:
+                iterable[k], iterable[k + 1] = iterable[k + 1], iterable[k]
+    
+    return iterable
