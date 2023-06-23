0f0330280897b26e8fb71aa63329ce451769a861
alex <alexvetrici@gmail.com>
2023-06-23 09:36:11 +0200

    heap sort function added

---
+++ sorting/heap-sort/heapSort.py
@@ -0,0 +1,29 @@
+def heapsort(iterable):
+    n = len(iterable)
+    build_max_heap(iterable)
+
+    for i in range(n - 1, 0, -1):
+        iterable[i], iterable[0] = iterable[0], iterable[i]
+        heapify(iterable, i, 0)
+
+def heapify(iterable, n, root):
+    left = 2 * root + 1  
+    right = 2 * root + 2
+    largest = root
+
+    if left < n and iterable[left] > iterable[root]:
+        largest = left
+
+    if right < n and iterable[right] > iterable[largest]:
+        largest = right
+
+    if largest != root:
+        iterable[largest], iterable[root] = iterable[root], iterable[largest]
+        heapify(iterable, n, largest)
+
+def build_max_heap(iterable):
+    n = len(iterable)
+    k = int(n // 2 - 1)
+
+    for i in range(k, -1, -1):
+        heapify(iterable, n, i)
