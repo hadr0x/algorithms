84e8cf61ed9032fa46ec6189dcf540e0b0889edf
alex <alexvetrici@gmail.com>
2023-06-23 07:46:52 +0200

    insertion sort added

---
+++ sorting/insertion-sort/insertionSort.py
@@ -0,0 +1,9 @@
+def insertionSort(iterable):
+    for i in range(len(iterable)):
+        k = i
+        
+        while k > 0 and iterable[k - 1] > iterable[k]:
+            iterable[k - 1],iterable[k] = iterable[k],iterable[k - 1]
+            k = k - 1
+    
+    return iterable
