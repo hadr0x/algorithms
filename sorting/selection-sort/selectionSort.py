20ac62baa9c84b340a744d6bcc3e462c18ca9b04
alex <alexvetrici@gmail.com>
2023-06-23 07:31:10 +0200

    selection sort added

---
+++ sorting/selection-sort/selectionSort.py
@@ -0,0 +1,9 @@
+def selectionSort(iterable):
+    for i in range(len(iterable) - 1):
+
+        for k in range(i, len(iterable)):
+
+            if iterable[k] < iterable[i]:
+                iterable[k], iterable[i] = iterable[i], iterable[k]
+
+    return iterable
