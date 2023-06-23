2c402c638d7b7732bc8965b9276b90f180001ea3
alex <alexvetrici@gmail.com>
2023-06-23 10:44:59 +0200

    quick sort added

---
+++ sorting/quick-sort/quickSort.py
@@ -0,0 +1,28 @@
+def partition(iterable, low, high):
+
+    # Rightmost element as pivot
+    pivot = iterable[high]
+
+    i = low - 1
+
+    for k in range(low, high):
+        if iterable[k] <= pivot:
+            i = i + 1
+            iterable[k], iterable[i] = iterable[i], iterable[k]
+
+    iterable[i + 1], iterable[high] = iterable[high], iterable[i + 1]
+
+    # Position where partition is done
+    return i + 1
+
+
+def quickSort(iterable, low, high):
+    if low < high:
+        
+        pi = partition(iterable, low, high)
+
+        # Recursive call on the left of pivot    
+        quickSort(iterable, low, pi - 1)
+
+        # Recursive call on the right of pivot
+        quickSort(iterable, pi + 1, high)
