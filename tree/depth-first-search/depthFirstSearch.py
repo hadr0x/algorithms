65b9a7f7d87828aa3a6de26af4d77cbfba508a97
alex <alexvetrici@gmail.com>
2023-06-29 19:06:48 +0200

    DFS algorithm added

---
+++ tree/depth-first-search/depthFirstSearch.py
@@ -0,0 +1,9 @@
+def DFS(visited, graph, node):
+
+    if node not in visited:
+        visited.append(node)
+        
+        for n in graph[node]:
+            DFS(visited, graph, n)
+
+    return visited
