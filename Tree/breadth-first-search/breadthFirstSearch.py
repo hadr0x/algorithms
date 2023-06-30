b55b62e1cfabb5384409d456f5ae0926bdf4156b
alex <alexvetrici@gmail.com>
2023-06-30 10:15:16 +0200

    BFS algorithm added

---
+++ Tree/breadth-first-search/breadthFirstSearch.py
@@ -0,0 +1,18 @@
+def BFS(root):
+    visited = []
+    queue = []
+
+    queue.append(root)
+
+    while queue:
+        node = queue.pop(0)
+        visited.append(node.data)
+
+        if node.left != None:
+            queue.append(node.left)
+        
+        if node.right != None:
+            queue.append(node.right)
+
+            
+    return visited
