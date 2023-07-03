c137f037bc1fd15281be73276832d9dfcf04ee27
alex <alexvetrici@gmail.com>
2023-07-03 09:07:00 +0200

    added traversal algorithms

---
+++ tree/tree-traversals/treeTraversals.py
@@ -0,0 +1,22 @@
+# Inorder Traversal : left --> root --> right
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data)
+        inorder(node.right)
+
+
+# Preorder Traversal : root --> left --> right
+def preorder(node):
+    if node:
+        print(node.data)
+        preorder(node.left)
+        preorder(node.right)
+
+
+# Postorder Traversal : left --> right --> root
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.data)
