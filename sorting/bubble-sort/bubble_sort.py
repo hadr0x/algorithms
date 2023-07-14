be75232e4ba197be4091121bfe816cb90b738268
alex <alexvetrici@gmail.com>
2023-07-14 14:46:41 +0200

    update bubble sort

--- sorting/bubble-sort/bubble_sort.py
+++ sorting/bubble-sort/bubble_sort.py
@@ -1,7 +1,7 @@
-def bubble_sort(A):
-    for i in range(len(A) - 1):
+def bubble_sort(array):
+    for i in range(len(array) - 1):
     
-        for k in range(0, len(A) - i - 1):
+        for k in range(0, len(array) - i - 1):
     
-            if A[k] > A[k + 1]:
-                A[k], A[k + 1] = A[k + 1], A[k]
+            if array[k] > array[k + 1]:
+                array[k], array[k + 1] = array[k + 1], array[k]
