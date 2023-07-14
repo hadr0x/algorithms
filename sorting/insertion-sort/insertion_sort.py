cb84a41f5ef4cad46b9c0c1c02af164e9e018bc4
alex <alexvetrici@gmail.com>
2023-07-14 14:48:58 +0200

    update insertion sort

--- sorting/insertion-sort/insertion_sort.py
+++ sorting/insertion-sort/insertion_sort.py
@@ -1,9 +1,7 @@
-def insertion_sort(iterable):
-    for i in range(len(iterable)):
+def insertion_sort(array):
+    for i in range(len(array)):
         k = i
         
-        while k > 0 and iterable[k - 1] > iterable[k]:
-            iterable[k - 1],iterable[k] = iterable[k],iterable[k - 1]
+        while k > 0 and array[k - 1] > array[k]:
+            array[k - 1],array[k] = array[k],array[k - 1]
             k = k - 1
-    
-    return iterable
