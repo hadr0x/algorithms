802befd116e13a531d837a65ac1f0bd4ea770a8b
alex <alexvetrici@gmail.com>
2023-07-14 14:14:06 +0200

    update dijkstra function

--- graph/dijkstra/dijkstra.py
+++ graph/dijkstra/dijkstra.py
@@ -8,27 +8,31 @@ class Graph:
 
     def add_edge(self, u, v, weight):
         self.edges[u][v] = weight
-        self.edges[v][u] = weight
+
+    def display_distances(self, dist):
+        for i in range(len(dist)):
+            print("Distance from vertex 0 to vertex", i, "is", dist[i])
 
 
-def dijkstra(graph, start_vertex):
-    D = {v:float('inf') for v in range(graph.v)}
-    D[start_vertex] = 0
+    def dijkstra(self, start_vertex):
+        dist = {v:float('inf') for v in range(self.v)}
+        dist[start_vertex] = 0
 
-    pq = PriorityQueue()
-    pq.put((0, start_vertex))
+        pq = PriorityQueue()
+        pq.put((0, start_vertex))
 
-    while not pq.empty():
-        (_, current_vertex) = pq.get()
-        graph.visited.append(current_vertex)
+        while not pq.empty():
+            (_, current_vertex) = pq.get()
+            self.visited.append(current_vertex)
 
-        for neighbor in range(graph.v):
-            if graph.edges[current_vertex][neighbor] != -1:
-                distance = graph.edges[current_vertex][neighbor]
-                if neighbor not in graph.visited:
-                    old_cost = D[neighbor]
-                    new_cost = D[current_vertex] + distance
-                    if new_cost < old_cost:
-                        pq.put((new_cost, neighbor))
-                        D[neighbor] = new_cost
-    return D
+            for neighbor in range(self.v):
+                if self.edges[current_vertex][neighbor] != -1:
+                    distance = self.edges[current_vertex][neighbor]
+                    if neighbor not in self.visited:
+                        old_cost = dist[neighbor]
+                        new_cost = dist[current_vertex] + distance
+                        if new_cost < old_cost:
+                            pq.put((new_cost, neighbor))
+                            dist[neighbor] = new_cost
+        
+        return self.display_distances(dist)
